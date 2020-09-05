e858df02189f708c917b29ad560e4a871f535e17
eli-alkorta <elialkor@gmail.com>
2020-09-05 15:49:15 +0200

    fix deployment

---
+++ web-build/precache-manifest.1a0c792db8717d10dc8322b36f315642.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "484c256f3a1693078383186a85c5e0ad",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/register-service-worker.js"
+  },
+  {
+    "revision": "d199bb9ad968f434e23e",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/static/js/app.c79a5383.chunk.js"
+  },
+  {
+    "revision": "57fa6caf9ad4faa93611",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/static/js/runtime~app.1876a360.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/static/js/2.b0a00d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0692b7143a2c4b1004fa",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/static/js/2.b0a00d87.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/serve.json"
+  },
+  {
+    "revision": "53348fa728fdb7d5f9a4d0ccb4cdd826",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/manifest.json"
+  },
+  {
+    "revision": "1585718ffa49a2070981f5eebaf5bd59",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/index.html"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/favicon.ico"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/favicon-32.png"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/favicon-16.png"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/eli-alkorta/gestor-de-tareas-para-movil/expo-service-worker.js"
+  }
+];
---
+++ web-build/register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/eli-alkorta/gestor-de-tareas-para-movil/expo-service-worker.js",{scope:"/eli-alkorta/gestor-de-tareas-para-movil/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
